09ba2d2f7094b9cdcced1356f358e4951650bb7c
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2021-01-18 23:58:29 -0500

    get job details from id

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,20 +14,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve static resources in client folder
 app.use("/",
-    express.static(path.join(__dirname,'../../client/dist'))
+  express.static(path.join(__dirname, '../../client/dist'))
 );
 
 // Set CORS policy
 app.use((inRequest: Request, inResponse: Response, inNext: NextFunction) => {
-    inResponse.header('Access-Control-Allow-Origin', '*');
-    inResponse.header('Access-Control-Allow-Methods', "GET,OPTIONS");
-    inResponse.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
-    inNext();
+  inResponse.header('Access-Control-Allow-Origin', '*');
+  inResponse.header('Access-Control-Allow-Methods', "GET,OPTIONS");
+  inResponse.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+  inNext();
 });
 
 // Start express app
 app.set('port', process.env.PORT || 3000);
-var server = app.listen(app.get('port'), function() {
+var server = app.listen(app.get('port'), function () {
   console.log(`Express server listening on `, server.address());
 });
 
@@ -36,45 +36,46 @@ var server = app.listen(app.get('port'), function() {
 
 // Get user info
 app.get('/api/user/:username',
-    async (inRequest: Request, inResponse: Response) => {
-        console.log(`GET /api/users/${inRequest.params.username}`);
-        try {
-            const response = await axios.get(`https://bio.torre.co/api/bios/${inRequest.params.username}`);
-            // inResponse.json(`GET /api/users/${inRequest.params.username}`);
-            inResponse.json(response.data);
-        } catch (error) {
-            console.log(error);
-            inResponse.send('error');
-        }
+  async (inRequest: Request, inResponse: Response) => {
+    console.log(`GET /api/users/${inRequest.params.username}`);
+    try {
+      const response = await axios.get(`https://bio.torre.co/api/bios/${inRequest.params.username}`);
+      inResponse.json(response.data);
+    } catch (error) {
+      console.log(error);
+      inResponse.send('error');
     }
+  }
 );
 
 // Search for jobs in general
 app.get('/api/jobs/search',
-    async (inRequest: Request, inResponse: Response) => {
-        console.log(`GET /api/jobs/search`);
-        try {
-          // Access the provided query parameters
-          let offset = inRequest.query.offset;
-          let size = inRequest.query.size;
-          let aggregate = inRequest.query.aggregate;
-          inResponse.json(`GET /api/jobs/search/?offset=${offset}&size=${size}&aggregate=${aggregate}`);
-        } catch (error) {
-            inResponse.send('error');
-        }
+  async (inRequest: Request, inResponse: Response) => {
+    console.log(`GET /api/jobs/search`);
+    try {
+      // Access the provided query parameters
+      let offset = inRequest.query.offset;
+      let size = inRequest.query.size;
+      let aggregate = inRequest.query.aggregate;
+      inResponse.json(`GET /api/jobs/search/?offset=${offset}&size=${size}&aggregate=${aggregate}`);
+    } catch (error) {
+      inResponse.send('error');
     }
+  }
 );
 
 
 // Get job info
 app.get('/api/jobs/:id',
-    async (inRequest: Request, inResponse: Response) => {
-        console.log(`GET /api/jobs/{inRequest.params.id}`);
-        try {
-            inResponse.json(`GET /api/jobs/${inRequest.params.id}`);
-        } catch (error) {
-            inResponse.send('error');
-        }
+  async (inRequest: Request, inResponse: Response) => {
+    console.log(`GET /api/jobs/${inRequest.params.id}`);
+    try {
+      const response = await axios.get(`https://torre.co/api/opportunities/${inRequest.params.id}`);
+      inResponse.json(response.data);
+    } catch (error) {
+      console.log(error);
+      inResponse.send('error');
     }
+  }
 );
 

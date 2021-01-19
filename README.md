c7d390293db150506bc5cbd5cbb9b0149c9dd737
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2021-01-18 23:37:48 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# README (and fill me!)
---
+++ server/src/index.ts
@@ -0,0 +1,76 @@
+import path from "path";
+import express, { Express, NextFunction, Request, Response } from "express";
+import bodyParser from 'body-parser';
+
+// Create Express app
+const app: Express = express();
+
+// Add json parsing middleware to app
+app.use(express.json());
+
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Serve static resources in client folder
+app.use("/",
+    express.static(path.join(__dirname,'../../client/dist'))
+);
+
+// Set CORS policy
+app.use((inRequest: Request, inResponse: Response, inNext: NextFunction) => {
+    inResponse.header('Access-Control-Allow-Origin', '*');
+    inResponse.header('Access-Control-Allow-Methods', "GET,OPTIONS");
+    inResponse.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+    inNext();
+});
+
+// Start express app
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+  console.log(`Express server listening on `, server.address());
+});
+
+
+// *** REST API endpoints *** //
+
+// Get user info
+app.get('/api/user/:username',
+    async (inRequest: Request, inResponse: Response) => {
+        console.log(`GET /api/users/${inRequest.params.username}`);
+        try {
+            inResponse.json(`GET /api/users/${inRequest.params.username}`);
+        } catch (error) {
+            inResponse.send('error');
+        }
+    }
+);
+
+// Search for jobs in general
+app.get('/api/jobs/search',
+    async (inRequest: Request, inResponse: Response) => {
+        console.log(`GET /api/jobs/search`);
+        try {
+          // Access the provided query parameters
+          let offset = inRequest.query.offset;
+          let size = inRequest.query.size;
+          let aggregate = inRequest.query.aggregate;
+          inResponse.json(`GET /api/jobs/search/?offset=${offset}&size=${size}&aggregate=${aggregate}`);
+        } catch (error) {
+            inResponse.send('error');
+        }
+    }
+);
+
+
+// Get job info
+app.get('/api/jobs/:id',
+    async (inRequest: Request, inResponse: Response) => {
+        console.log(`GET /api/jobs/{inRequest.params.id}`);
+        try {
+            inResponse.json(`GET /api/jobs/${inRequest.params.id}`);
+        } catch (error) {
+            inResponse.send('error');
+        }
+    }
+);
+

58892b98d816f770a59a12a522841663a6767031
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2021-01-19 22:10:43 -0500

    match endopoint / POST

--- ml-server/app.py
+++ ml-server/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello From Data Analyser!"
+
+@app.route('/match', methods=['POST'])
+def match():
+    json_ = request.get_json(force=True)
+    print(json_)
+
+    return jsonify({'status': 'ok'})
+
+
 
 if __name__ == '__main__':
     app.run()
